fae39461c7de9e8d03fb9db248b6834a31a7522a
Javier Moncada <moncadajavier2@yahoo.com>
2023-02-19 14:30:18 -0500

    Initial commit

---
+++ remappedSrc/net/mexicanminion/itemfinder/ItemFinderClient.java
@@ -0,0 +1,10 @@
+package net.mexicanminion.itemfinder;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ItemFinderClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/mexicanminion/itemfinder/ItemFinder.java
@@ -0,0 +1,16 @@
+package net.mexicanminion.itemfinder;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ItemFinder implements ModInitializer {
+
+	public static final String MOD_ID = "item-finder";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
